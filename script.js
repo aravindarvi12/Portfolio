d0dd9baa6f8b27e3c51ef759226dbd53070365e4
aravindarvi12 <aravindarvi12gmail.com>
2024-12-27 10:14:15 +0530

    Portfolio/landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu_link");
+  const menu_icon = document.querySelector(".icon");
+  menu.classList.toggle("open")
+  menu_icon.classList.toggle("open")
+}
